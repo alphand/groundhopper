526a22d879f90cc3888b455d182898cb589a7ead
Niko Darmawan <darmawan.niko@gmail.com>
2016-04-13 18:28:44 +1000

    login form validation

--- src/containers/accounts/login.vue
+++ src/containers/accounts/login.vue
@@ -1,36 +1,51 @@
 <template>
   <div class="login-panel">
-    <div class="row">
-      <div class="medium-6 medium-centered large-4 large-centered columns">
+    <div class="row align-center">
+      <div class="medium-6 large-4 column">
         <validator name="loginValidator">
-          <form class="row column log-in-form"
+          <form class="log-in-form"
             v-on:submit.prevent="onSubmit">
-          <h4>Log in with your email!</h4>
-          <div class="form-group">
-            <label for="email">Email address:</label>
-            <input type="text" id="email" name="email"
-              v-model="email"
-              v-validate:username="{required:true}"/>
-          </div>
-          <div class="form-group">
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password"
-              v-model="password"/>
-          </div>
-          <div class="form-group">
-            <input type="checkbox" id="show-password">
-            <label for="show-password">show password</label>
-          </div>
-          <div class="form-actions">
-            <div class="errors">
-              <p v-if="$loginValidator.username.required">Email is required</p>
+            <h4>Log in with your email!</h4>
+            <div class="form-group">
+              <label for="email">
+                Email address:
+                <input type="text" id="email" name="email"
+                  v-model="email"
+                  v-validate:username="{required:true}"/>
+              </label>
             </div>
-            <button type="submit" class="button expanded">Submit</button>
-          </div>
-          <div class="form-group">
-            <a href="">Forgot your password?</a>
-          </div>
-        </form>
+            <div class="form-group">
+              <label for="password" >
+                Password:
+                <input type="password" id="password" name="password"
+                  v-model="password"
+                  v-bind:type="(isPassShown)? 'text':'password'"
+                  v-validate:password="{required:true,minLength:6}"/>
+              </label>
+              <p class="help-text">
+                Your password must have at least 6 characters
+              </p>
+            </div>
+            <div class="form-group">
+              <input type="checkbox" id="show-password" v-model="isPassShown" />
+              <label for="show-password">
+                show password
+              </label>
+            </div>
+            <div class="form-actions">
+              <div class="errors">
+                <p class="form-error"
+                  v-if="$loginValidator.username.dirty &&
+                  $loginValidator.username.required">
+                  Email is required
+                </p>
+              </div>
+              <button type="submit" class="button expanded">Submit</button>
+            </div>
+            <div class="form-group">
+              <a href="">Forgot your password?</a>
+            </div>
+          </form>
         </validator>
       </div>
     </div>
@@ -42,13 +57,14 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      isPassShown: false
     }
   },
 
   methods: {
     onSubmit () {
-      console.log('submitting-data', this.email, this.password)
+      console.log('submitting-data', this.email, this.password, this.$loginValidator)
     }
   }
 }
@@ -56,6 +72,7 @@ export default {
 
 <style>
   .log-in-form {
+    margin-top: 30px;
     border: 1px solid #cacaca;
     padding: 1rem;
     border-radius: 3px;
